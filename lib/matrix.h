825b791fac215839a320c16cfeb8941490905bf1
allEyezOnCode <alexandredewilde2001@gmail.com>
2023-04-27 16:56:09 +0200

    add missing include and add new functions definitions

--- lib/matrix.h
+++ lib/matrix.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "state.h"
+
 typedef enum {
 	BFM_MATRIX_KIND_FULL,
 	BFM_MATRIX_KIND_BAND,
@@ -12,7 +14,17 @@ typedef enum {
 
 typedef struct {
 	double* data; // don't access this directly; there are accessor functions for this
+	double** a;
 } bfm_matrix_full_t;
+
+typedef struct {
+	size_t m;
+	size_t n;
+	size_t k;
+
+	double* data;
+	double** a;
+} bfm_matrix_band_t;
 
 typedef struct {
 	bfm_state_t* state;
@@ -20,11 +32,15 @@ typedef struct {
 	bfm_matrix_kind_t kind;
 	bfm_matrix_major_t major;
 
-	size_t n;
 	size_t m;
+	size_t n;
 
 	union {
 		bfm_matrix_full_t full;
 		bfm_matrix_band_t band;
 	};
 } bfm_matrix_t;
+
+int bfm_allocate_matrix_full(bfm_matrix_full_t *full_matrix);
+
+int bfm_free_matrix_full(bfm_matrix_full_t *full_matrix);
--- lib/state.h
+++ lib/state.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdlib.h>
 // XXX separate file?
 
 typedef struct {
