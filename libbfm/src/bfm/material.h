0e1fa52536fe619123bdce4dabdd14a07fd4fff2
obiwac <obiwac@gmail.com>
2023-05-03 19:29:46 +0200

    libbfm/material: Create `bfm_material_t` object

---
+++ libbfm/src/bfm/material.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <bfm/bfm.h>
+
+typedef struct {
+	double r;
+	double g;
+	double b;
+	double a;
+} bfm_colour_t;
+
+typedef struct {
+	bfm_state_t* state;
+
+	char* name;
+	bfm_colour_t colour;
+
+	double rho; // density
+	double E; // Young's modulus (at room temperature)
+	double nu; // Poisson's ratio
+} bfm_material_t;
+
+int bfm_material_create(bfm_material_t* material, bfm_state_t* state, char* name, double rho, double E, double nu);
+int bfm_material_destroy(bfm_material_t* material);
+
+int bfm_material_set_colour(bfm_material_t* material, double r, double g, double b, double a);
---
+++ libbfm/src/material.c
@@ -0,0 +1,36 @@
+#include <string.h>
+
+#include <bfm/material.h>
+
+int bfm_material_create(bfm_material_t* material, bfm_state_t* state, char* name, double rho, double E, double nu) {
+	memset(material, 0, sizeof *material);
+
+	material->state = state;
+	material->name = strdup(name);
+
+	if (!material->name)
+		return -1;
+
+	material->rho = rho;
+	material->E = E;
+	material->nu = nu;
+
+	return 0;
+}
+
+int bfm_material_destroy(bfm_material_t* material) {
+	bfm_state_t* const state = material->state;
+
+	state->free(material->name);
+
+	return 0;
+}
+
+int bfm_material_set_colour(bfm_material_t* material, double r, double g, double b, double a) {
+	material->colour.r = r;
+	material->colour.g = g;
+	material->colour.b = b;
+	material->colour.a = a;
+
+	return 0;
+}
