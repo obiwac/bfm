0fb729fc4adde8d6cef61b3bb3c35f8a38608757
obiwac <obiwac@gmail.com>
2023-05-04 19:30:10 +0200

    pybfm/instance: `add_condition` for... adding conditions

--- pybfm/bfm/instance.py
+++ pybfm/bfm/instance.py
@@ -1,4 +1,5 @@
 from .libbfm import lib, ffi
+from .condition import Condition
 from .obj import Obj
 from .state import default_state
 
@@ -11,3 +12,6 @@ class Instance:
 
 	def __del__(self):
 		assert not lib.bfm_instance_destroy(self.c_instance)
+
+	def add_condition(self, condition: Condition)
+		assert not lib.bfm_instance_add_condition(self.c_instance, condition.c_condition
