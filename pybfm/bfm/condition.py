8e1577dc5c8f8db2cdd7e5abf881dc12161bd03b
obiwac <obiwac@gmail.com>
2023-05-07 16:18:03 +0200

    pybfm/condition: Make distinction between Dirichlet & Von Neumann boundary conditions

--- pybfm/bfm/condition.py
+++ pybfm/bfm/condition.py
@@ -5,9 +5,12 @@ from .state import default_state
 from collections.abc import Callable
 
 class Condition:
-	def __init__(self, mesh: Mesh):
+	DIRICHLET = 0
+	VON_NEUMANN = 1
+
+	def __init__(self, mesh: Mesh, kind: int):
 		self.c_condition = ffi.new("bfm_condition_t*")
-		assert not lib.bfm_condition_create(self.c_condition, default_state, mesh.c_mesh)
+		assert not lib.bfm_condition_create(self.c_condition, default_state, mesh.c_mesh, kind)
 
 		self.mesh = mesh
 
