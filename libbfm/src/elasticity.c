9fbca6e1eae483e618df58d7d2646fa9245e65cf
obiwac <obiwac@gmail.com>
2023-05-07 12:30:11 +0200

    libbfm/elasticity: Style changes

--- libbfm/src/elasticity.c
+++ libbfm/src/elasticity.c
@@ -20,18 +20,18 @@ static int get_local_element(bfm_mesh_t* mesh, size_t e, bfm_local_element_t* el
 }
 
 static int build_elasticity_system_local(bfm_local_element_t* element, bfm_instance_t* instance, bfm_force_t** forces, size_t n_forces, bfm_system_t* system, double a, double b, double c) {
-	bfm_obj_t* obj = instance->obj;
-    bfm_mesh_t* mesh = obj->mesh;
+	bfm_obj_t* const obj = instance->obj;
+   bfm_mesh_t* const mesh = obj->mesh;
 
 	double* const x = element->x;
 	double* const y = element->y;
 
-    bfm_rule_t* rule = &mesh->rule;
+	bfm_rule_t* const rule = &mesh->rule;
     
-	bfm_shape_functions_t* shape = &mesh->functions;
+	bfm_shape_functions_t* const shape = &mesh->functions;
     
-	bfm_matrix_t* A = system->A;
-    bfm_vec_t* B = system->B;
+	bfm_matrix_t* const A = system->A;
+	bfm_vec_t* const B = system->B;
 
 	size_t* const map = element->map;
 
@@ -50,7 +50,7 @@ static int build_elasticity_system_local(bfm_local_element_t* element, bfm_insta
 		double const eta = rule->eta[i];
         
 		double phi[4];
-        shape->get_phi(xsi, eta, phi);
+		shape->get_phi(xsi, eta, phi);
 		
 		double dphi_dxsi[4];
 		double dphi_deta[4];
@@ -69,7 +69,7 @@ static int build_elasticity_system_local(bfm_local_element_t* element, bfm_insta
 			dy_deta += y[k] * dphi_deta[k];
 		}
 
-		double det_jacobian = fabs(dx_dxsi*dy_deta - dx_deta*dy_dxsi);
+		double const det_jacobian = fabs(dx_dxsi * dy_deta - dx_deta * dy_dxsi);
 
 		double dphi_dx[4];
 		double dphi_dy[4];
@@ -81,10 +81,12 @@ static int build_elasticity_system_local(bfm_local_element_t* element, bfm_insta
 
 		for (size_t j = 0; j < element->n_local_nodes; j++) {
 			int const index_i = 2 * map[j];
+
 			for (size_t k = 0; k < n_forces; k++) {
-				bfm_force_t* force = forces[k];
+				bfm_force_t* const force = forces[k];
 				bfm_force_eval(force, &pos, &force_on_point);
-            	B->data[index_i + 0] = det_jacobian * weight * force_on_point.data[0] * obj->material->rho * phi[i];
+
+				B->data[index_i + 0] = det_jacobian * weight * force_on_point.data[0] * obj->material->rho * phi[i];
 				B->data[index_i + 1] = det_jacobian * weight * force_on_point.data[1] * obj->material->rho * phi[i];
 			}
 		}
@@ -100,13 +102,14 @@ static int build_elasticity_system_local(bfm_local_element_t* element, bfm_insta
 				double const f_21 = b * dphi_dy[j] * dphi_dx[k] + c * dphi_dx[j] * dphi_dy[k];
 				double const f_22 = a * dphi_dy[j] * dphi_dy[k] + c * dphi_dx[j] * dphi_dx[k];
 
-                bfm_matrix_add(A, index_i, index_j, det_jacobian * weight * f_11);
-                bfm_matrix_add(A, index_i, index_j + 1, det_jacobian * weight * f_11);
-                bfm_matrix_add(A, index_i + 1, index_j, det_jacobian * weight * f_21);
-                bfm_matrix_add(A, index_i + 1, index_j + 1, det_jacobian * weight * f_22);
+				bfm_matrix_add(A, index_i, index_j, det_jacobian * weight * f_11);
+				bfm_matrix_add(A, index_i, index_j + 1, det_jacobian * weight * f_11);
+				bfm_matrix_add(A, index_i + 1, index_j, det_jacobian * weight * f_21);
+				bfm_matrix_add(A, index_i + 1, index_j + 1, det_jacobian * weight * f_22);
 			}
 		}
 	}
+
 	return 0;
 }
 
@@ -117,19 +120,21 @@ static int apply_constrain(bfm_system_t* system, size_t node, double value) {
 		system->B->data[i] -= value;// * bfm_matrix_get(system->A, i, node);
 		bfm_matrix_set(system->A, i, node, 0.);
 	}
+
 	for (size_t i = 0; i < system->A->m; i++) {
 		bfm_matrix_set(system->A, node, i, 0.);
 	}
+
 	bfm_matrix_set(system->A, node, node, 1.);
 	system->B->data[node] = value;
 }
 
 int bfm_build_elasticity_system(bfm_instance_t* instance, bfm_force_t** forces, size_t n_forces, bfm_system_t* system) {
-	bfm_obj_t* obj = instance->obj;
-	bfm_mesh_t* mesh = obj->mesh;
-    bfm_material_t* material = obj->material;
+	bfm_obj_t* const obj = instance->obj;
+	bfm_mesh_t* const mesh = obj->mesh;
+   bfm_material_t* const material = obj->material;
 
-    double const a = material->E / (1 - material->nu * material->nu);
+   double const a = material->E / (1 - material->nu * material->nu);
 	double const b = material->E * material->nu / (1 - material->nu * material->nu);
 	double const c = material->E / (2 * (1 + material->nu));
 
@@ -139,14 +144,14 @@ int bfm_build_elasticity_system(bfm_instance_t* instance, bfm_force_t** forces, 
 
 	for (size_t i = 0; i < mesh->n_elems; i++) {
 		get_local_element(mesh, i, &element);
-        build_elasticity_system_local(&element, instance, forces, n_forces, system, a, b, c);
+		build_elasticity_system_local(&element, instance, forces, n_forces, system, a, b, c);
 	}
 
 	for (size_t i = 0; i < instance->n_conditions; i++) {
 		bfm_condition_t* const condition = instance->conditions[i];
 		for (size_t j = 0; j < mesh->n_nodes; j++) {
 			if (condition->nodes[j]) {
-				// TODO deal with dirilect != 0
+				// TODO deal with Dirichlet != 0
 				apply_constrain(system, j * 2, 0);
 				apply_constrain(system, j * 2 + 1, 0);
 			}
