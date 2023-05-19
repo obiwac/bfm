ec135b938f5a810f1f6877b9f6eb025a347dc8d2
obiwac <obiwac@gmail.com>
2023-05-19 07:17:40 +0200

    examples/deformation: Proper bridge and conditions

--- examples/deformation.py
+++ examples/deformation.py
@@ -8,10 +8,14 @@ from bfm import Bfm, Condition, Force_linear, Instance, Mesh_lepl1110, Mesh_wave
 # create initial BFM context
 # TODO should this be renamed something a little clearer, e.g. Scene?
 
+print("Starting BFM")
+
 bfm = Bfm()
 
 # create a 2D simplex mesh
 # this is simply a horizontal bar 1 unit wide and 0.2 units tall
+
+print("Loading meshes")
 
 # mesh = Mesh(2, Mesh.SIMPLEX)
 #
@@ -22,16 +26,60 @@ terrain = Mesh_wavefront("meshes/terrain.obj", True)
 bfm.add_scenery(terrain)
 
 # mesh = Mesh_lepl1110("meshes/8.lepl1110")
-mesh = Mesh_wavefront("meshes/test.obj")
+mesh = Mesh_wavefront("meshes/bridge.obj")
 
 # create Dirichlet boundary conditions for mesh
 # add all nodes close to the centre
 
+cross = []
+
+def cache_cross():
+	print("Caching cross-section")
+
+	for i in range(terrain.c_mesh.n_nodes):
+		tz = terrain.c_mesh.coords[i * 3 + 2]
+
+		if abs(tz) > 0.04:
+			continue
+
+		tx = terrain.c_mesh.coords[i * 3 + 0]
+		ty = terrain.c_mesh.coords[i * 3 + 1]
+
+		cross.append((tx, ty))
+
+	with open("meshes/cross.py", "w") as f:
+		f.write(f"{cross}")
+
+def read_cross():
+	global cross
+
+	with open("meshes/cross.py", "r") as f:
+		cross = eval(f.read())
+
+# cache_cross()
+read_cross()
+
+def is_boundary(mesh, coord):
+	x, y = coord
+	z = 0 # all coordinates are gonna be aligned at z = 0
+
+	smallest_diff = (999, 0)
+
+	for tx, ty in cross:
+		diff = abs(tx - x)
+
+		if diff < smallest_diff[0]:
+			smallest_diff = (diff, ty)
+
+	return y < smallest_diff[1]
+
+print("Computing boundary conditions")
+
 boundary_condition_x = Condition(mesh, Condition.DIRICHLET_X)
-boundary_condition_x.populate(lambda mesh, coord: any(x < -0.499 for x in coord))
+boundary_condition_x.populate(is_boundary)
 
 boundary_condition_y = Condition(mesh, Condition.DIRICHLET_Y)
-boundary_condition_y.populate(lambda mesh, coord: any(x < -0.499 for x in coord))
+boundary_condition_y.populate(is_boundary)
 
 # create object out of 7075-series aluminium:
 # density (rho): 2.81 g/cm^3
@@ -58,6 +106,8 @@ gravity = Force_linear.EARTH_GRAVITY_2D
 
 # create simulation
 # run the simulation
+
+print("Run simulation")
 
 sim = Sim(Sim.PLANAR_STRESS)
 
